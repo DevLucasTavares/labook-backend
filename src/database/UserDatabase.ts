26372dc7effb31cf9a4211d67fd1721f97f7ef90
Carrarinii <lucas.c.d.tavares@gmail.com>
2023-05-06 10:12:42 -0300

    endpoint-feito

--- src/database/UserDatabase.ts
+++ src/database/UserDatabase.ts
@@ -1,7 +1,14 @@
+import { UserDB } from "../models/User";
 import { BaseDatabase } from "./BaseDatabase";
 
 export class UserDatabase extends BaseDatabase {
 
     public static TABLE_USERS = "users"
+
+    public insertUser = async (userDB: UserDB): Promise<void> => {
+        await BaseDatabase
+        .connection(UserDatabase.TABLE_USERS)
+        .insert(userDB)
+    }
 
 }
--- src/dtos/user/signup.dto.ts
+++ src/dtos/user/signup.dto.ts
@@ -14,4 +14,4 @@ export const SignupSchema = z.object({
     name: z.string().min(2),
     email: z.string().email(),
     password: z.string().min(4)    
-})
+}).transform(data => data as SignupInputDTO)
