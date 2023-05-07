93dce4193e3b5e939569d31d27282576c7e483b5
Carrarinii <lucas.c.d.tavares@gmail.com>
2023-05-07 07:53:40 -0300

    endpoint-feito

--- src/router/postRouter.ts
+++ src/router/postRouter.ts
@@ -1,3 +1,18 @@
 import express from 'express'
+import { PostController } from '../controller/PostController'
+import { PostBusiness } from '../business/PostBusiness'
+import { PostDatabase } from '../database/PostDatabase'
+import { IdGenerator } from '../services/IdGenerator'
+import { TokenManager } from '../services/TokenManager'
 
 export const postRouter = express.Router()
+
+const postController = new PostController(
+    new PostBusiness(
+        new PostDatabase(),
+        new IdGenerator(),
+        new TokenManager()
+    )
+)
+
+postRouter.post("/", postController.createPost)
